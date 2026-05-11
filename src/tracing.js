368fc2da1c3e37bbf883991b032263b91714c1c5
spoved-aws <kanukhosla10@gmail.com>
2026-05-11 01:22:50 +0000

    update for tracing 3

--- src/tracing.js
+++ src/tracing.js
@@ -3,27 +3,22 @@
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
-
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
 
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 
-// OTLP exporter → Jaeger OTLP endpoint
-const traceExporter = new OTLPTraceExporter({
-  url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+const exporter = new OTLPTraceExporter({
+  url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT
 });
 
 const sdk = new NodeSDK({
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]:
-      process.env.OTEL_SERVICE_NAME || 'nodejs-app-a',
+    [SemanticResourceAttributes.SERVICE_NAME]: process.env.OTEL_SERVICE_NAME || 'nodejs-app-a',
   }),
-
-  traceExporter,
-
+  traceExporter: exporter,
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
 sdk.start();
 
-console.log('✅ OpenTelemetry tracing initialized (OTLP → Jaeger)');
+console.log('OpenTelemetry tracing initialized (OTLP → Jaeger)');
